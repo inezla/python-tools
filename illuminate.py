01f0f1f1d097279b337c1902948ab8ed90054669
Inez Lynch Alfaro <inezla@tekeli-li.local>
2015-09-20 21:12:15 -0700

    first commit

---
+++ illuminate.py
@@ -0,0 +1,10 @@
+#this script will allow me to automatically scrape (get info) my grades from the illuminate website
+
+#go to https://smmusd.illuminatehc.com/login
+#under username type 596125
+#under password type 123456
+#scrape grades
+#print to screen
+
+
+#in further use, try to send text of grades
