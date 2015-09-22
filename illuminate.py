89540fd31caa99dade5c33207bfd07a444fe7dad
Inez Lynch Alfaro <inezla@tekeli-li.local>
2015-09-21 17:01:21 -0700

    I just figured out requests

--- illuminate.py
+++ illuminate.py
@@ -1,4 +1,10 @@
 #this script will allow me to automatically scrape (get info) my grades from the illuminate website
+import requests
+
+rr = requests.get(myurl, auth = (user, passwd))
+user = "596125"
+passwd = "123456"
+myurl = "https://smmusd.illuminatehc.com/login"
 
 #go to https://smmusd.illuminatehc.com/login
 #under username type 596125
