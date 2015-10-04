3d0140712cc18ca71878f9a51efbd9ec559fce65
Inez Lynch Alfaro <inezla@tekeli-li.local>
2015-10-04 12:07:36 -0700

    still trying to log in

--- illuminate.py
+++ illuminate.py
@@ -1,10 +1,17 @@
 #this script will allow me to automatically scrape (get info) my grades from the illuminate website
 import requests
 
-rr = requests.get(myurl, auth = (user, passwd))
 user = "596125"
 passwd = "123456"
 myurl = "https://smmusd.illuminatehc.com/login"
+#rr = requests.get(myurl, auth = (user, passwd))
+#session = requests.session
+payload_login = {
+'username': 596125,
+'password': 123456
+}
+with requests.Session() as ss:
+	con = ss.post("https://smmusd.illuminatehc.com/login")
 
 #go to https://smmusd.illuminatehc.com/login
 #under username type 596125
