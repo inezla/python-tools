e4ada2e1c93f66ebc8a4351dbaaffa9bf6de9f1c
Michael Alfaro <michaelalfaro@ucla.edu>
2015-10-04 15:29:44 -0700

    tump

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+/* Lesson 1: Intro to Javascript
+ * 
+ *
+ *
+ */
+
+// Change this to your name!
+var name = "oogla";
+var age = "100";
+// You can use other vars to make a var using +
+var message = "hi. my name is " + name + " and I am " + age + "!";
+
+// Display your message
+alert(message);
